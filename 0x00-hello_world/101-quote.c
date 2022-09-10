319e95df8f6bcfb2fd343f2c55440fbdc5980ce8
CHIZLIFE <engrrichman@gmail.com>
2022-09-09 21:49:58 -0700

    Task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,13 @@
 /**
 * main - print the string in the puts function
 *
-*Description: using the main function
-*this programs prints "and that piece of art is useful" - Dora korpar, 2015-10-19"
+* Description: using the main function
+* this programs prints "and that piece of art is useful - Dora korpar, 2015-10-19"
 * Result: 0
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 61);
+	fwrite(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 61);
+	
 	return (1);
 }
