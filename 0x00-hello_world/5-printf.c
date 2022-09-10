917ada6865b507e840aed0de7694393db3d064cb
CHIZLIFE <engrrichman@gmail.com>
2022-09-09 19:43:02 -0700

    Task 5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the puts function 
 *
-*Description: using the main function
+* Description: using the main function
 * this program prints "programming is like building a multilingual puzzule"
 * Return: 0
 */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
