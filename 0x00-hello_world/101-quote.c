bad81fddd795f5b3c76a5c2bbd9b918624ecf17f
CHIZLIFE <engrrichman@gmail.com>
2022-09-09 21:38:57 -0700

    Task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the puts function
+*
+*Description: using the main function
+*this programs prints "and that piece of art is useful" - Dora korpar, 2015-10-19"
+* Result: 0
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 61);
+	return (1);
+}
