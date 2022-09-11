4f58e84b5e8d5122fcc2a0372fab3fd5b4e47120
CHIZLIFE <engrrichman@gmail.com>
2022-09-10 23:38:26 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+
+int n = 10;
+
+if (n=10) {
+printf("is positive");
+}
+
+if (n=0) {
+printf("is zero);
+}
+
+if (n<0) {
+printf("is negative");
+}
+
+return 0;
+
+}
