b569bbdd9b4a6a59909c5d742e1c5b7f77508dda
CHIZLIFE <engrrichman@gmail.com>
2022-09-11 20:22:13 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,11 @@
  */
 int main(void)
 {
-	char ch e, q;
+	char ch = e, q;
 
-	e = 'e';
-	q = 'q';
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-		if (ch ! = e && ch ! = q)
-			putchar(ch);
+	if (ch != e && ch != q)
+	{putchar(ch); }
 	putchar("\n");
 	return (0);
 }
