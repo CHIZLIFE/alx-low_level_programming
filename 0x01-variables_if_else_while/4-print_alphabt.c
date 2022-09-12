b9d5349ace35d7e4499701f1d18c5d17c1c6e652
CHIZLIFE <engrrichman@gmail.com>
2022-09-11 19:47:17 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch e, q;
+
+	e = 'e';
+	q = 'q';
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		if (ch ! = e && ch ! = q)
+			putchar(ch);
+	putchar("\n");
+	return (0);
+}
