b0357b12daedf34f0bbeb28bd6b9a6dd58852616
CHIZLIFE <engrrichman@gmail.com>
2022-09-12 21:20:39 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
-* main - print the string in the put function
+* main - Entry point
 *
 * Description: using the main function
 * this program prints "programming is like building a multilingual puzzle"
-* Return: 0
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 
 	char charType;
 	int intType;
-	long longType;
+	double doubleType;
 	float floatType;
-	
-	//sizeof evaluates the size of variable
+
+	/*sizeof evaluates the size of variable */
 	printf("Size of char: %zu byte\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of double: %zu bytes\n", sizeof(doubleType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
 
-	retutn (0);
+	return (0);
 }
