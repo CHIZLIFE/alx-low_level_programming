3931a43388f946671bf4f0034ede4fd38f8506e8
CHIZLIFE <engrrichman@gmail.com>
2022-09-12 17:35:49 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		puthchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putcha(letter);
+
+	putchar('\n');
+
+	return (0);
+}
