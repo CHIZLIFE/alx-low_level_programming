5275fdbeee0f3e9df122926b7ff1c461457904d9
CHIZLIFE <engrrichman@gmail.com>
2022-09-17 04:54:28 -0700

    Task 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 			_putchar(' ');
 			product = numb + mult;
 
-			if (product <= 9)
+			if ((product / 10) > 0)
 				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
