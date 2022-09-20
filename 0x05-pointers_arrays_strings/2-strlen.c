78aaa2ec8d962909aa52a9d9ca6a27eae9e6222a
CHIZLIFE <engrrichman@gmail.com>
2022-09-20 13:45:54 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the lenght of a string
+ * @s: input
+ *
+ * Return: 0 on success
+ */
+int _strlen(char *s);
+{
+	int count = 0, i = 0;
+	char name[30];
+
+	while (name(i) != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
