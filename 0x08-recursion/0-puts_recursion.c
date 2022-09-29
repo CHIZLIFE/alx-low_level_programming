10358f1811e8aa5fa93c84c5797bd36e683b14ed
CHIZLIFE <engrrichman@gmail.com>
2022-09-29 15:28:12 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,15 @@
  * _puts_recursion - function that print a string, followed by a new line.
  * @s: string
  *
- * Return: on success 1
- * on error 0, is returned
+ * Return: no return
  */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
-
+	{
 	putchar(*s);
 	_puts_recursion(s + 1);
-
+	}
+	else
 	putchar('\n');
 }
